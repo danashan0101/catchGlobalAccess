f954fb1cd4260a098ce2301106d423c11c111e9a
Viatcheslav Zadorozhniy <danashan0101@gmail.com>
2017-05-18 13:53:49 +0300

    Update catchGlobalAccess.js

--- catchGlobalAccess.js
+++ catchGlobalAccess.js
@@ -1,8 +1,21 @@
+/*
+  Author: Viatcheslav Zadorozhniy
+
+  catchGlobalAccess method takes one parameter:
+  it can be a string (name of global variable to catch),
+  object (rule set of showing information about catch) with the following parameters:
+    * varName - name of global variable to catch (string);
+    * message - a message that will be shown when the global access will be caught (string);
+    * warnAlways - true, if you want showing message each time when the global access will be caught (boolean);
+    * showCaller - true, if you want showing the method which made an access to the global variable (boolean);
+
+  or an array of objects or strings
+*/
+
 (function (globalScope) {
     var defaultWarnMessage = 'Accessing {0} through the global scope is deprecated.',
         hasConsole = typeof console !== 'undefined',
         hasProxy = typeof Proxy !== 'undefined',
-        warnAlways = false,
         wrapped = {};
 
     function wrap(varName, msg, warnAlways, showCaller) {
@@ -13,11 +26,11 @@
 
         function showWarn(msg, fn) {
             if (hasConsole && (firstWarn || warnAlways)) {
+                console.warn(msg);
+
                 if (showCaller && fn) {
                     console.log(fn.caller);
                 }
-
-                console.warn(msg);
             }
             firstWarn = false;
         }
@@ -51,7 +64,7 @@
         wrapped[varName] = true;
     }
 
-    function catchGlobalAccess(toCatch, showCaller) {
+    function catchGlobalAccess(toCatch) {
         var itemsToCatch = [];
 
         if (Array.isArray(toCatch)) {
@@ -67,14 +80,14 @@
                 varName = item.varName || item;
 
                 if (globalScope[varName] && !wrapped[varName]) {
-                    wrap(varName, item.message || defaultWarnMessage.replace('{0}', varName), item.warnAlways || warnAlways, showCaller);
+                    wrap(varName, item.message || defaultWarnMessage.replace('{0}', varName), item.warnAlways, item.showCaller);
                 }
             }
         });
     }
 
     if (typeof define === 'function' && define.amd) {
-        define('catchGlobalAccess', function () {
+        define(function () {
             return catchGlobalAccess;
         });
     } else {
